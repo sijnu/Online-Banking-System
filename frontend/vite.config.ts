ef88e48383519ffb2ea29431db772b0573269504
SomaSekharMarella <2300030411@kluniversity.in>
2025-09-23 01:27:31 +0530

    added disr folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+	  base: '/onlinebanking/', // This ensures all asset paths are prefixed correctly
+
 	plugins: [react()],
 	server: {
 		port: 5173,
@@ -10,6 +12,5 @@ export default defineConfig({
 			'/admin': 'http://localhost:8080',
 			'/user': 'http://localhost:8080'
 		}
-	},
-	base:'/onlinebanking/',
+	}
 }) 
