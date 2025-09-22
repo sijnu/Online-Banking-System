fbcf9b157f68a45c137e82ccb836d066826368b1
SomaSekharMarella <2300030411@kluniversity.in>
2025-09-23 01:38:35 +0530

    added jenkins

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	  base: '/onlinebanking/', // This ensures all asset paths are prefixed correctly
+  base: '/onlinebanking/',
 
 	plugins: [react()],
 	server: {
